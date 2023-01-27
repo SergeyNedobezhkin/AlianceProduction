f6caf62a68f6d0e812934babcd295dc3b3cc9c19
Sergey Nedobezhkin <zelenka1633@gmail.com>
2023-01-27 19:46:17 +0400

    Сверстал блок blog

--- js/main.js
+++ js/main.js
@@ -81,3 +81,19 @@ const swiperSteps = new Swiper(".steps-slider", {
     prevEl: ".step-button-prev",
   },
 });
+
+const swiperBlog = new Swiper(".blog-slider", {
+  slidesPerView: 1,
+  speed: 400,
+  spaceBetween: 30,
+  loop: true,
+  breakpoints: {
+    1241: {
+      slidesPerView: 2,
+    },
+  },
+  navigation: {
+    nextEl: ".blog-button-next",
+    prevEl: ".blog-button-prev",
+  },
+});
