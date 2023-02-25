d2f122b9f0d2a4904f052daa0182a033bfb873bb
Sergey Nedobezhkin <zelenka1633@gmail.com>
2023-02-26 00:00:03 +0400

    Сделал шаблоны блоков сайта и начал делать из них второстепенные страницы

---
+++ template-parts/blog-slider-block.php
@@ -0,0 +1,65 @@
+<section class="section blog">
+  <div class="container">
+    <?php
+    if (!empty($block_title_blogMore)) {
+      echo ' <div class="seporator"></div>';
+      echo '<h2 class="section-title blog-section-title">' . $block_title_blogMore . '</h2>';
+    }
+    if (!empty($block_title_blogMore_two)) {
+      echo ' <div class="seporator"></div>';
+      echo '<h2 class="section-title blog-section-title">' . $block_title_blogMore_two . '</h2>';
+    }
+    if (!empty($block_title_blogMore_about)) {
+      echo ' <div class="seporator"></div>';
+      echo '<h2 class="section-title blog-section-title">' . $block_title_blogMore_about . '</h2>';
+    }
+    ?>
+    <?php
+    if (!empty($block_title_blogMore_text_left && $block_title_blogMore_text_right)) {
+      echo '<div class="blog-section-wrapper">
+      <p class="blog-section-text-right blog-section-wrapper">' . $block_title_blogMore_text_right . '</p>
+      <p class="blog-section-text-left blog-section-wrapper">' . $block_title_blogMore_text_left . '</p>
+      </div>';
+    } ?>
+    <div class="swiper blog-slider">
+      <div class="swiper-wrapper">
+        <a href="./blogMore.php" class="swiper-slide blog-card"><img src="img/blog/blog-photo.jpg" alt="blog-photo"
+            class="blog-card-image">
+
+          <h3 class="blog-card-title">Современная методология разработки одухотворила всех причастных</h3>
+          <p class="blog-card-text">Действия представителей оппозиции, превозмогая сложившуюся непростую экономическую
+            ситуацию, в равной степени предоставлены...</p>
+        </a>
+
+        <a href="./blogMore.php" class="swiper-slide blog-card"><img src="img/blog/blog-post.jpg" alt="blog-post"
+            class="blog-card-image">
+
+          <h3 class="blog-card-title">Сложно сказать, почему жизнь прекрасна</h3>
+          <p class="blog-card-text">Сложно сказать, почему элементы политического процесса функционально разнесены на
+            независимые элементы. Безусловно, высокотехнологичная...</p>
+        </a>
+      </div>
+
+      <div class="blog-slider-footer">
+        <?php
+        if (!empty($block_title_blogMore)) {
+          echo '<a href="./blog.php" class="button-link blog-button-link">Весь блог</a>';
+        } ?>
+
+        <div class="blog-buttons primary-buttons-wrapper">
+          <div class="blog-button-prev primary-button-prev">
+            <svg width="36" height="24">
+              <use href="./img/sprite.svg#arrow-prev"></use>
+            </svg>
+          </div>
+
+          <div class="blog-button-next primary-button-next">
+            <svg width="36" height="24">
+              <use href="./img/sprite.svg#arrow-next"></use>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ template-parts/section-light-block.php
@@ -0,0 +1,55 @@
+<section class="section section-light">
+  <div class="container">
+    <div class="seporator"></div>
+    <h2 class="section-title">схема работы</h2>
+    <div class="swiper steps-slider">
+      <ol class="swiper-wrapper steps">
+        <li class="swiper-slide steps-item">
+          <span class="steps-num"><strong>01</strong></span>
+          <h3 class="steps-title">Знакомство</h3>
+          <p class="steps-text">
+            Безусловно, сплочённость команды профессионалов позволяетоценить значение форм воздействия.
+          </p>
+          <a href="#" class="button-link">Оставить заявку</a>
+        </li>
+        <li class="swiper-slide steps-item">
+          <span class="steps-num"><strong>02</strong></span>
+          <h3 class="steps-title">Заключение договора</h3>
+          <p class="steps-text">
+            Лишь интерактивные прототипы </br>призваны к ответу.
+          </p>
+
+        </li>
+        <li class="swiper-slide steps-item">
+          <span class="steps-num"><strong>03</strong></span>
+          <h3 class="steps-title">Производство</h3>
+          <p class="steps-text">
+            А также стремящиеся вытеснить традиционное производство,
+            нанотехнологии функционально </br>разнесены на независимые</br> элементы.
+          </p>
+        </li>
+        <li class="swiper-slide steps-item">
+          <span class="steps-num"><strong>04</strong></span>
+          <h3 class="steps-title">Доставка</h3>
+          <p class="steps-text">
+            В частности, экономическая повестка сегодняшнего дня говорит о</br> возможностях приоритизации</br> разума
+            над эмоциями.
+          </p>
+        </li>
+      </ol>
+      <div class="steps-buttons primary-buttons-wrapper">
+        <div class="step-button-prev primary-button-prev">
+          <svg width="36" height="24">
+            <use href="./img/sprite.svg#arrow-prev"></use>
+          </svg>
+        </div>
+
+        <div class="step-button-next primary-button-next">
+          <svg width="36" height="24">
+            <use href="./img/sprite.svg#arrow-next"></use>
+          </svg>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
