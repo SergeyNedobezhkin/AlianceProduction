eaad106a252dd4bb15b4dc9e90341071f4a4620b
Sergey Nedobezhkin <zelenka1633@gmail.com>
2023-03-07 00:44:24 +0400

    Полностью сверстал страницу about и адаптировал под мобильную версию

---
+++ template-parts/ourProduction.php
@@ -0,0 +1,66 @@
+<div class="section  clients-laboratoryWorker">
+
+  <div class="container clients-laboratoryWorker-container">
+    <div class="clients-wrapper">
+      <div class="clients-contant">
+        <div class="seporator clients-seporator"></div>
+        <h2 class="section-title clients-title clients-title-laboratoryWorker">Наше производство</h2>
+        <picture class="clients-photo"></picture>
+        <!-- <source type="image/webp" srcset="img/found.webp"> -->
+        <source type="image/png" srcset="img/laboratoryWorker.png">
+        <img src="/img/laboratoryWorker.png" alt="laboratoryWorker" class="clients-photo">
+        </picture>
+        <div class="clients-text">
+          <p class="clients-text-1">Предварительные выводы неутешительны: разбавленное изрядной долей эмпатии,
+            рациональное мышление обеспечивает широкому кругу (специалистов) участие в формировании глубокомысленных
+            рассуждений. Но граница обучения кадров создаёт необходимость включения в производственный план целого ряда
+            внеочередных мероприятий с учётом комплекса кластеризации усилий.</p>
+          <p class="clients-text-2">Реализация намеченных плановых заданий, а также свежий взгляд на привычные вещи -
+            безусловно открывает новые горизонты для соответствующих условий активизации. Предварительные выводы
+            неутешительны: экономическая повестка сегодняшнего дня требует анализа анализа существующих паттернов
+            поведения.</p>
+        </div>
+        <ul class="clients-list">
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use href="./img/sprite.svg#car"></use>
+            </svg>
+            Автомобильная химия
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use href="./img/sprite.svg#home"></use>
+            </svg>
+            Бытовая химия
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use href="./img/sprite.svg#dis"></use>
+            </svg>
+            Дезинфицирующие средства
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use href="./img/sprite.svg#aerozol"></use>
+            </svg>
+            Пищевые аэрозоли
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use href="./img/sprite.svg#cosmetic"></use>
+            </svg>
+            Косметическая продукция
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use href="./img/sprite.svg#brush"></use>
+            </svg>
+            Краски аэрозольные
+          </li>
+        </ul>
+
+      </div>
+    </div>
+  </div>
+</div>
+</section>
